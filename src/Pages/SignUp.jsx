9cdb8767ef4ace35cf74edc3d9fbc23d70dabfe9
Ansharg <anshgarg94161@gmail.com>
2023-09-24 00:15:38 +0530

    responsive changes

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -45,14 +45,14 @@ export default function SignUp() {
           }
     }
   return (
-    <section className=' w-screen h-screen bg-grid p-16'>
+    <section className=' w-screen h-screen bg-grid min-[400px]:p-16'>
         <div className='flex h-full w-full box-border items-center justify-center'>
-            <div className='w-1/3 h-4/5 bg-[#23262d] rounded-lg'>
+            <div className='w-screen sm:w-1/2 lg:w-1/3 lg:h-4/5 bg-[#23262d] rounded-lg'>
                 <div>
-                    <div className='text-white font-bold text-3xl p-5 text-center mt-3'>
+                    <div className='text-white font-bold md:text-xl lg:text-3xl p-5 text-center mt-3'>
                         <p>Sign Up</p>
                     </div>
-                    <form className='mt-10 px-5' onSubmit={onSubmit}>
+                    <form className=' xl:mt-10 px-5' onSubmit={onSubmit}>
                         {/* <div className=''>
                             <input type="text" className='h-12 w-full rounded-lg px-5 focus:border-black focus:border-2 ' placeholder='Enter Your Email...' />
                         </div>
@@ -62,7 +62,7 @@ export default function SignUp() {
                         <div className='mt-10 box-border'>
                             <button type="submit" className=' bg-orange-600 text-white capitalize w-full py-3 font-bold text-xl rounded-lg hover:border-2 hover:border-orange-600 hover:bg-transparent'>submit</button>
                         </div> */}
-                        <div className='flex justify-between text-lg font-semibold space-x-4'>
+                        <div className='flex justify-between lg:text-lg font-semibold space-x-4'>
                             <div className='flex flex-col'>
                                 <label htmlFor="First" className='text-white'>First Name</label>
                                 <input type="First" id="First" className=' w-11/12 h-10 rounded-md outline-none focus:border-2 focus:border-orange-600' value={First} onChange={onchange}/>
@@ -72,19 +72,19 @@ export default function SignUp() {
                                 <input type="Last" id="Last"  className=' w-11/12 h-10 rounded-md outline-none focus:border-2 focus:border-orange-600' value={Last} onChange={onchange} />
                             </div>
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col lg:text-lg'>
                             <label htmlFor="email" className='text-white font-semibold text-xl'>Email</label>
                             <input type="email" id="email" className=' h-10 rounded-md outline-none focus:border-2 focus:border-orange-600' value={email} onChange={onchange}/>
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col lg:text-lg'>
                             <label htmlFor="password" className='text-white font-semibold text-xl'>Password</label>
                             <input type="password" id="password" className=' h-10 rounded-md outline-none focus:border-2 focus:border-orange-600' value={password} onChange={onchange}/>
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col lg:text-lg'>
                             <label htmlFor="number" className='text-white font-semibold text-xl'>Mobile Number</label>
                             <input type="number" id="number" className=' h-10 rounded-md outline-none focus:border-2 focus:border-orange-600' value={number} onChange={onchange}/>
                         </div>
-                        <div className='mt-10'>
+                        <div className='my-5 '>
                             <button type='submit' className='w-full py-2 bg-orange-600 text-white font-semibold text-lg font-serif hover:bg-transparent hover:border-2 hover:border-orange-600 rounded-md'>Submit</button>
                         </div>
                     </form>
